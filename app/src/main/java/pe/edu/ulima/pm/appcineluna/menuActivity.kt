4b2753d68781b2c7a1af366e3b11275416245323
Fabián Gonzales <luminition@hotmail.com>
2022-05-20 13:16:47 -0500

    Update 2

---
+++ app/src/main/java/pe/edu/ulima/pm/appcineluna/menuActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.ulima.pm.appcineluna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
