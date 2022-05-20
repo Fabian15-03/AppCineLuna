209254d36e086bcbbd6696001fddd996a909080d
Fabián Gonzales <luminition@hotmail.com>
2022-05-20 18:16:58 -0500

    Update 4

---
+++ app/src/main/java/pe/edu/ulima/pm/appcineluna/fragments/Fragment2.kt
@@ -0,0 +1,18 @@
+package pe.edu.ulima.pm.appcineluna.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import pe.edu.ulima.pm.appcineluna.R
+
+class Fragment2:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment2, container, false)
+    }
+}
